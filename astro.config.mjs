f199017ff0ccf1b9d8ecb672323df5ea10fe383d
henrique919 <henrysestak@gmail.com>
2026-06-26 10:53:34 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  site: 'https://cleanruniq.com',
+  integrations: [
+    tailwind(),
+    mdx(),
+  ],
+  output: 'static',
+  trailingSlash: 'always',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+});
 
 // https://astro.build/config
 export default defineConfig({
